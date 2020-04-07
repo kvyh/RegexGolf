927d9959d816e8377811ff1164c9d335ee238960
Karl <kvyh@users.noreply.github.com>
2020-04-07 16:29:56 -0700

    Changed default problem loading to access default_problems.json in the github.com repository.

--- app/src/main/java/com/kvyh/regexgolf/MainActivity.java
+++ app/src/main/java/com/kvyh/regexgolf/MainActivity.java
@@ -8,13 +8,19 @@ import androidx.room.Room;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -40,42 +46,40 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         adapter.reload();
-        Log.i("problemsJSONfile", "problems loaded: " + String.valueOf(adapter.problems.size()));
         if (adapter.problems.size() == 0){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
             addDefaultProblems();}
     }
+    public RequestQueue requestQueue;
 
-    private void addDefaultProblems() {
-        try {
-            String fileString = "";
-            InputStreamReader reader = new InputStreamReader(getResources().openRawResource(R.raw.default_problems));
-            int read = 1;
-            while ((read > -1)) {
-                read = (int) reader.read();
-                if (read > -1) {
-                    fileString += (char) read;
-                }
+    public void addDefaultProblems() {
+        //requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://kvyh.github.io/RegexGolf/app/src/main/res/raw/default_problems.json";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray array = response.getJSONArray("problems");
+                    for (int i = 0; i < array.length(); i += 1) {
+                        JSONObject prob = array.getJSONObject(i);
+                        Problem tempProblem = new Problem(prob.getInt("id"),
+                                prob.getString("title"),
+                                prob.getInt("difficulty"),
+                                prob.getInt("shortest"),
+                                prob.getString("target"),
+                                prob.getString("reject"),
+                                prob.getString("source"));
+                        tempProblem.addToDB();
+                    }
+                } catch (JSONException e) { Log.e("problemsJSONfile", e.getMessage()); }
             }
-
-            JSONObject json = new JSONObject(fileString);
-            JSONArray array = json.getJSONArray("problems");
-            for (int i = 0; i < array.length(); i += 1) {
-                JSONObject prob = array.getJSONObject(i);
-                Problem tempProblem = new Problem(prob.getInt("id"),
-                                                prob.getString("title"),
-                                                prob.getInt("difficulty"),
-                                                prob.getInt("shortest"),
-                                                prob.getString("target"),
-                                                prob.getString("reject"),
-                                                prob.getString("source"));
-                tempProblem.addToDB();
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VolleyJSONfile", error.getMessage());
             }
-
-        }  catch(JSONException e) {
-            Log.e("problemsJSONfile", e.getMessage());
-        }  catch(IOException e) {
-            Log.e("problemsJSONfile", e.getMessage());
-        }
+        });
+        requestQueue.add(request);
         adapter.reload();
     }
 }
